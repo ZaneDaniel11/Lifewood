41c141abf4f20d4ba88a50a205e1d40ea005eeb5
Zane Daniel <118499857+ZaneDaniel11@users.noreply.github.com>
2025-03-14 10:25:57 +0800

    Impliment React Redux

---
+++ Portfolio/src/assets/services/HandleApplicationsApi.js
@@ -0,0 +1,19 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+
+export const HandleApplicationsApi = createApi({
+    reducerPath: 'HandleApplicationsApi',
+    baseQuery: fetchBaseQuery({baseUrl:"http://localhost:5237/api/ApplicationsApi/",
+    }),
+    endpoints: (builder) => ({
+        GetApplication: builder.query({query:() => "GetApplications"}),
+        CreateApplication: builder.mutation({
+            query:(NewApplication) => ({
+                url: "InsertApplication",
+                method: "POST",
+                body: NewApplication,
+            })
+        }),
+    })
+})
+
+export const {useGetApplicationQuery,useCreateApplicationMutation} = HandleApplicationsApi;
---
+++ Portfolio/src/assets/store.js
@@ -0,0 +1,7 @@
+import {configureStore} from "@reduxjs/toolkit";
+import {setupListeners} from "@reduxjs/toolkit/query";
+import {} from "./assets/services/HandleApplicationsApi.js"
+
+const store = configureStore({
+    reducer: {},
+})   
